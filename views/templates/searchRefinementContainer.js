696eaac3a31a4ba4c22402beec76b67afd8d1b60
tabhan <tabhan@aaxischina.com>
2018-03-28 18:33:14 +0800

    11

--- views/templates/searchRefinementContainer.js
+++ views/templates/searchRefinementContainer.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import contentMsg from '../../lib/utils/contentMsg';
 
-export default props => <div></div>
+export default props => {
+
+    const msgFilter = contentMsg('ProductListPage', 'Label.PLP.Filter.Description', 'Me gustar&#237;a');
+
+    return <section className="refinement container jq-refinement">
+        <div className="text-center title">
+            <span dangerouslySetInnerHTML={{__html: `
+                <a data-toggle="collapse" href="javascript:void(0)" title="${msgFilter}"
+                   data-target="#coll-subtitle" aria-expanded="false">
+                    ${msgFilter}
+                    <span class="icon-plus-circle-empty jq-refinement-plus hidden-xs" id="openRefinementButton"
+                          title="${contentMsg('GlobalCartridges', 'ALT.PLP.ExpandDemention')}"></span>
+                </a>
+            `}}>
+            </span>
+        </div>
+    </section>
+}
 
